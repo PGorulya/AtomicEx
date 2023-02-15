134e4b7f51cde2d64f9185cff9a882f97576c556
PGorulya <pavel.gorulya@gmail.com>
2023-02-15 07:00:08 +0100

    initial commit

---
+++ src/main/java/com/example/AtomicEx.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
+public class AtomicEx {
+
+    static AtomicInteger counter = new AtomicInteger(0);
+    static AtomicIntegerArray atomicArray = new AtomicIntegerArray(40);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Start values of atomicArray:");
+        System.out.println(atomicArray.toString());
+        System.out.println();
+
+        Thread t1 = new Thread(new M1());
+        Thread t2 = new Thread(new M1());
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Result values of atomicArray: ");
+        System.out.println(atomicArray.toString());
+
+    }
+}
+
+class M1 implements Runnable {
+
+    @Override
+    public void run() {
+
+        AtomicEx.counter.get();     // need it !
+
+        while (AtomicEx.counter.incrementAndGet() < 40) {
+            AtomicEx.atomicArray.set(AtomicEx.counter.get(), AtomicEx.counter.get());
+            System.out.println("Thread: " + Thread.currentThread().getId() + " index = " + AtomicEx.counter.get() + "  counter = " + AtomicEx.counter.get());
+        }
+    }
+}
